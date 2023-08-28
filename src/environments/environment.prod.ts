57731ab9971c039540ab5aecf79eb7e494a488c5
Manoj Karkera <manoj.karkera@gmail.com>
2023-08-28 11:32:43 +0100

    env prod variable added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://customer-api-management.azure-api.net',
+  apiDeveloperUrl: 'https://customer-api-management.developer.azure-api.net'
 };
